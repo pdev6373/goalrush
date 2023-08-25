a46f1a1ab1c1bdfcf8516cde78f391e3943e89ed
pdev6373 <pdev6373@gmail.com>
2023-08-25 11:20:55 +0100

    make video card

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { Videos } from "@/components";
 import { Videos as AllVideos } from "@/constants";
 import styles from "./page.module.css";
@@ -6,12 +7,14 @@ export default function Video() {
   return (
     <div className={styles.wrapper}>
       {AllVideos.map((video) => (
-        <Videos
-          thumbnail={video.thumbnail}
-          date={video.date}
-          title={video.title}
-          route="/video/hello"
-        />
+        <Fragment key={video.title}>
+          <Videos
+            thumbnail={video.thumbnail}
+            date={video.date}
+            title={video.title}
+            route="/video/hello"
+          />
+        </Fragment>
       ))}
     </div>
   );
