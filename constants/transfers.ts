a1f1bda96bd50a097c8691b87834c3520a044374
pdev6373 <pdev6373@gmail.com>
2023-08-26 15:32:20 +0100

    Versioning untracked files

---
+++ constants/transfers.ts
@@ -0,0 +1,198 @@
+import { TransferType } from "@/types";
+
+const Transfer: TransferType[] = [
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+  {
+    category: "Latest Transfers",
+    body: [
+      {
+        image: "",
+        age: 21,
+        name: "Neco Williams",
+        position: "Defender",
+        reason: "End of Loan",
+        nationality: {
+          country: "UAE",
+          logo: "",
+        },
+        previousClub: {
+          logo: "",
+          name: "Fullham ",
+        },
+        currentClub: {
+          logo: "",
+          name: "Liverpool",
+        },
+      },
+    ],
+  },
+];
+
+export default Transfer;
